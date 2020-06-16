a6d492f2db93c9d756172a723c09e40d79286269
donaldlee725 <donaldlee725725@gmail.com>
2020-06-16 23:34:45 +0800

    COVID19

--- covid19.py
+++ covid19.py
@@ -10,33 +10,27 @@ import json
 import pandas as pd
 from datetime import date, timedelta
 
-today = date.today()
-d1 = today.strftime("%Y-%m-%d")
+today = date.today() - timedelta(days=1)
 begin = date(2020, 1, 21)
 
-def daterange(begin, end):
-    for n in range(int((end - begin).days)):
-        yield begin + timedelta(n)
-        
-def dataCollection():
-    dfList = list()
-    for day in daterange(begin, today):
-        url = "https://covid-19-data.p.rapidapi.com/report/country/name"
-        
-        querystring = {"date-format":"YYYY-MM-DD","format":"json","date": day,"name":"China"}
-        
-        headers = {
-            'x-rapidapi-host': "covid-19-data.p.rapidapi.com",
-            'x-rapidapi-key': "14614902a9mshf485023edcb0d11p1e441ejsn9f15496c7dfb"
-            }
-        
-        response = json.loads(json.dumps(requests.request("GET", url, headers=headers, params=querystring).json(), indent=4))
-        df = pd.DataFrame(response[0].get("provinces"))
-        dfList.append(df)
-    df = pd.concat(dfList)
-    return df
-
-dataCollection()
-
-
+def dateRange(begin, end):
+    dateDifference = (end - begin).days
+    for n in range(dateDifference):
+        yield (begin + timedelta(days=n)).strftime("%Y-%m-%d")
     
+
+df = pd.DataFrame()
+for day in dateRange(begin, today):
+    url = "https://covid-19-data.p.rapidapi.com/report/country/name"
+    
+    querystring = {"date-format":"YYYY-MM-DD","format":"json","date": day,"name":"China"}
+    
+    headers = {
+        'x-rapidapi-host': "covid-19-data.p.rapidapi.com",
+        'x-rapidapi-key': "14614902a9mshf485023edcb0d11p1e441ejsn9f15496c7dfb"
+        }
+    
+    response = json.loads(json.dumps(requests.request("GET", url, headers=headers, params=querystring).json(), indent=4))
+    df.append(response[0].get("provinces"))
+
+
